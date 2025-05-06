6409682c2b425987575d9c304883d06c4ffbac81
slhun22 <105125575+slhun22@users.noreply.github.com>
2025-05-07 00:18:45 +0900

    1644 : prime + two pointer

--- 1644.cpp
+++ 1644.cpp
@@ -1,40 +1,40 @@
 #include<iostream>
-#include<stack>
+#include<vector>
+#include<bitset>
 using namespace std;
 
-int cache[1001][1001] = { 0, };
+bitset<4000001> b;
+vector<int> cumulsum;
 
 int main() {
 	cin.tie(0);
-	cout.tie(0);
 	ios::sync_with_stdio(0);
-	int i, j;
-	string a, b;
-	stack<char> st;
- 	cin >> a >> b;
-	for (i = 0; i < a.length(); i++) {
-		for (j = 0; j < b.length(); j++) {
-			if (a[i] == b[j])
-				cache[i+1][j+1] = cache[i][j] + 1;
-			else
-				cache[i+1][j+1] = max(cache[i+1][j], cache[i][j+1]);
-		}
+	int i,j,N, l, r, sum, cnt = 0;
+	cin >> N;
+	b.set(1);
+	for (i = 2; i <= N; i++) {
+		if (b[i]) continue;
+		for (j = i * 2; j <= N; j += i)
+			b.set(j);
 	}
-	cout << cache[i][j]<<"\n";
-	while (cache[i][j]) {
-		if (a[i-1] == b[j-1]) {
-			st.push(a[i-1]);
-			i--; j--;
+	cumulsum.push_back(0);
+	for (i = 2; i <= N; i++) {
+		if (!b[i])
+			cumulsum.push_back(cumulsum.back() + i);
+	}
+	l = r = 1;
+	int size = cumulsum.size();		
+	while (size) {
+		sum = cumulsum[r] - cumulsum[l - 1];
+		if (sum <= N) {
+			if (sum == N) cnt++;
+			if (r + 1 < size) r++;
+			else break;
 		}
 		else {
-			if (cache[i-1][j] <= cache[i][j-1])
-				j--;
-			else
-				i--;
+			if (l + 1 < size) l++;
+			else break;
 		}
 	}
-	while (!st.empty()) {
-		cout << st.top();
-		st.pop();
-	}
+	cout << cnt;
 }
